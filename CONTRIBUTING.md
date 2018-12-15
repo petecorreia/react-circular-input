d5f3703f8fae723854c463646c73fee6d8b069d1
Pete Correia <pete@petecorreia.com>
2018-12-15 18:35:09 +0000

    Typescript version

---
+++ CONTRIBUTING.md
@@ -0,0 +1,16 @@
+# Contributing
+
+Issues and pull requests are welcome.
+
+Please **be nice** and read the following before contributing.
+
+- Test before submitting pull requests - `npm test`
+
+## Overview
+
+Folders:
+
+- `/src` source code
+- `/tests` tests, including snapshots
+
+Please ensure to test any new code added, and update snapshots when relevant.
---
+++ src/useCircularDrag.tsx
@@ -0,0 +1,113 @@
+import { useState, MouseEvent, TouchEvent } from "react";
+import {
+  Coordinates,
+  stopEvent,
+  calculateNearestValueToPoint,
+  clamp,
+  absMousePos,
+  absTouchPos
+} from "./utils";
+
+type useCircularDragArgs = {
+  value: number;
+  radius: number;
+  center: Coordinates;
+  minValue: number;
+  maxValue: number;
+  getContainerPosition: () => Coordinates;
+  onDragEnd: (value: number) => {};
+};
+
+export function useCircularDrag({
+  value,
+  radius,
+  center,
+  minValue,
+  maxValue,
+  getContainerPosition,
+  onDragEnd
+}: useCircularDragArgs) {
+  const [dragValue, setDragValue] = useState<number | null>(null);
+  const [isDragging, setDragging] = useState(false);
+
+  function handleMouseDown(e: Event | MouseEvent) {
+    stopEvent(e);
+    setDragging(true);
+    addMouseEventListeners();
+  }
+
+  function handleTouchStart(e: Event | TouchEvent) {
+    stopEvent(e);
+    setDragging(true);
+    addTouchEventListeners();
+  }
+
+  function handleMouseMove(e: Event | MouseEvent) {
+    stopEvent(e);
+    const pointerPos = absMousePos(e as MouseEvent);
+    setEventValue(pointerPos);
+  }
+
+  function handleTouchMove(e: Event | TouchEvent) {
+    stopEvent(e);
+    const pointerPos = absTouchPos(e as TouchEvent);
+    setEventValue(pointerPos);
+  }
+
+  function handleMouseUp(e: Event | MouseEvent) {
+    stopEvent(e);
+    onEventEnd();
+    removeMouseEventListeners();
+  }
+
+  function handleTouchEnd(e: Event | TouchEvent) {
+    stopEvent(e);
+    onEventEnd();
+    removeTouchEventListeners();
+  }
+
+  function addMouseEventListeners() {
+    document.addEventListener("mousemove", handleMouseMove, { passive: false });
+    document.addEventListener("mouseup", handleMouseUp, { passive: false });
+  }
+
+  function addTouchEventListeners() {
+    document.addEventListener("touchmove", handleTouchMove, { passive: false });
+    document.addEventListener("touchend", handleTouchEnd, { passive: false });
+  }
+
+  function removeMouseEventListeners() {
+    document.removeEventListener("mousemove", handleMouseMove);
+    document.removeEventListener("mouseup", handleMouseUp);
+  }
+
+  function removeTouchEventListeners() {
+    document.removeEventListener("touchmove", handleTouchMove);
+    document.removeEventListener("touchend", handleTouchEnd);
+  }
+
+  function onEventEnd() {
+    onDragEnd(typeof dragValue === "number" ? dragValue : value);
+    setDragging(false);
+    setDragValue(null);
+  }
+
+  function setEventValue(pointerPos: Coordinates) {
+    const nearestValue = calculateNearestValueToPoint({
+      center,
+      container: getContainerPosition(),
+      point: pointerPos,
+      radius,
+      value
+    });
+    const newValue = clamp(minValue, maxValue, nearestValue);
+    setDragValue(newValue);
+  }
+
+  return {
+    dragValue,
+    isDragging,
+    handleMouseDown,
+    handleTouchStart
+  };
+}
