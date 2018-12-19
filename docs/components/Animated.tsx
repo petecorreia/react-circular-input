2dc9ad5f67c9c1fc78291870becc1050a01faa43
Pete Correia <pete@petecorreia.com>
2018-12-19 18:23:27 +0000

    Animated example works but needs better supporting utils

---
+++ docs/components/Animated.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useSpring, animated, interpolate } from 'react-spring/hooks'
+import { useCircularThumbDrag, useCircularInputContext } from '../../src'
+import { polarToCartesian, valueToAngle, DEG_360_IN_RAD } from '../../src/utils'
+
+export const AnimatedThumb = () => {
+	const { value, center, radius } = useCircularInputContext()
+	const dragProps = useCircularThumbDrag()
+	const { value: animatedValue } = useSpring({ value })
+
+	return (
+		<animated.circle
+			r={20}
+			fill="#0045e5"
+			{...dragProps}
+			cx={interpolate([animatedValue], v => {
+				const { x } = polarToCartesian({
+					center,
+					angle: valueToAngle(v),
+					radius,
+				})
+				return x
+			})}
+			cy={interpolate([animatedValue], v => {
+				const { y } = polarToCartesian({
+					center,
+					angle: valueToAngle(v),
+					radius,
+				})
+				return y
+			})}
+		/>
+	)
+}
+
+export const AnimatedProgress = () => {
+	const { value, radius, center } = useCircularInputContext()
+	const innerCircumference = DEG_360_IN_RAD * radius
+	const {
+		value: animatedValue,
+		innerCircumference: animatedCircumference,
+	} = useSpring({ innerCircumference, value })
+
+	return (
+		<animated.circle
+			fill="none"
+			stroke="#3D99FF"
+			strokeWidth={20}
+			strokeLinecap="round"
+			cx={center.x}
+			cy={center.y}
+			r={radius}
+			strokeDasharray={animatedCircumference}
+			strokeDashoffset={interpolate(
+				[animatedValue, animatedCircumference],
+				(v, c) => c * (1 - v)
+			)}
+			transform={`rotate(-90 ${center.x} ${center.y})`}
+		/>
+	)
+}
