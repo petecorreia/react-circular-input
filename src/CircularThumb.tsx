d1610a3302bd178692afb23089626f5fe93ddbb9
Pete Correia <pete@petecorreia.com>
2019-02-19 11:54:35 +0000

    Add transition to Thumb

--- src/CircularThumb.tsx
+++ src/CircularThumb.tsx
@@ -3,16 +3,24 @@ import { useCircularInputContext, useCircularDrag } from './'
 
 export const CircularThumb = (props: SVGProps<SVGCircleElement>) => {
 	const { getPointFromValue, isFocused, setFocused } = useCircularInputContext()
+
 	const point = getPointFromValue()
 	if (!point) return null
 	const { x, y } = point
+
 	const ref = useRef<SVGCircleElement | null>(null)
 	const { isDragging } = useCircularDrag(ref)
+
+	const style = {
+		transition: 'r 150ms cubic-bezier(0.215, 0.61, 0.355, 1)',
+		...(props.style || {}),
+	}
 	return (
 		<circle
-			r={isFocused || isDragging ? 25 : 20}
+			r={isFocused || isDragging ? 23 : 20}
 			fill="#0045e5"
 			{...props}
+			style={style}
 			ref={ref}
 			cx={x}
 			cy={y}
