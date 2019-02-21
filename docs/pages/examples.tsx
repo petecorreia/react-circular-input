46c9b2ec41dd4161f6f83940836443efe3e1f685
Pete Correia <pete@petecorreia.com>
2019-02-21 13:37:02 +0000

    Add min, max and scale example

--- docs/pages/examples.tsx
+++ docs/pages/examples.tsx
@@ -11,137 +11,6 @@ import {
 } from '../../src'
 import styled from 'styled-components'
 
-const DefaultExample = () => {
-	const [value, setValue] = useState(0.25)
-	return (
-		<CircularInput value={value} onChange={setValue}>
-			<CircularTrack />
-			<CircularProgress />
-			<CircularThumb />
-		</CircularInput>
-	)
-}
-
-const ProgressExample = () => {
-	const [value, setValue] = useState(0.25)
-
-	useEffect(() => {
-		const i = setInterval(() => {
-			setValue(Math.random())
-		}, 1000)
-		return () => clearInterval(i)
-	}, [])
-
-	return (
-		<Spring to={{ value }} config={config.slow}>
-			{props => (
-				<CircularInput value={props.value}>
-					<CircularTrack strokeWidth={5} stroke="#eee" />
-					<CircularProgress stroke={`hsl(${props.value * 100}, 100%, 50%)`} />
-				</CircularInput>
-			)}
-		</Spring>
-	)
-}
-
-const StyledCircularInput = styled(CircularInput)`
-	.custom-progress {
-		stroke: #b1108e;
-		stroke-width: 40px;
-	}
-`
-
-const StyledCircularTrack = styled(CircularTrack)`
-	stroke: #f6f6f6;
-	stroke-width: 10px;
-`
-
-const StyledExample = () => {
-	const [value, setValue] = useState(0.25)
-	return (
-		<StyledCircularInput value={value} onChange={setValue}>
-			<StyledCircularTrack />
-			<CircularProgress className="custom-progress" />
-			<CircularThumb r={10} fill="rgba(255,255,255,0.5)" />
-		</StyledCircularInput>
-	)
-}
-
-const AnimatedExample = () => {
-	const [value, setValue] = useState(0.25)
-
-	return (
-		<Spring to={{ value }} config={config.stiff}>
-			{props => (
-				<CircularInput value={props.value} onChange={setValue}>
-					<CircularTrack />
-					<CircularProgress />
-					<CircularThumb />
-				</CircularInput>
-			)}
-		</Spring>
-	)
-}
-
-const CustomComponent = () => {
-	const { getPointFromValue, value } = useCircularInputContext()
-	const { x, y } = getPointFromValue()
-
-	return (
-		<text
-			x={x}
-			y={y}
-			textAnchor="middle"
-			dy="0.35em"
-			fill="rgb(61, 153, 255)"
-			style={{ pointerEvents: 'none', fontWeight: 'bold' }}
-		>
-			{Math.round(value * 100)}
-		</text>
-	)
-}
-
-const CustomExample = () => {
-	const [value, setValue] = useState(0.25)
-
-	return (
-		<Spring to={{ value }} config={config.stiff}>
-			{props => (
-				<CircularInput value={props.value} onChange={setValue}>
-					<CircularProgress
-						strokeWidth={45}
-						stroke={`rgba(61, 153, 255, ${props.value})`}
-					/>
-					<CircularThumb
-						fill="white"
-						stroke="rgb(61, 153, 255)"
-						strokeWidth="5"
-					/>
-					<CustomComponent />
-				</CircularInput>
-			)}
-		</Spring>
-	)
-}
-
-const RenderPropExample = () => {
-	const [value, setValue] = useState(0.25)
-	return (
-		<CircularInput value={value} onChange={setValue}>
-			{({ getPointFromValue }) => (
-				<>
-					<CircularTrack />
-					<CircularProgress />
-					<text {...getPointFromValue()} dx="30" dy="0.3em">
-						wee!
-					</text>
-					<CircularThumb />
-				</>
-			)}
-		</CircularInput>
-	)
-}
-
 export default () => (
 	<>
 		<Lead>
@@ -321,6 +190,62 @@ export default () => (
 			`}
 		/>
 
+		<h2 id="min-max-scale">Min, Max & Scale</h2>
+
+		<p>
+			There are no props for min/max/etc as you can easily achieve these and
+			much more with simple code.
+		</p>
+
+		<p>
+			There are no props for min/max/etc as you can easily achieve these and
+			much more with simple code.
+		</p>
+
+		<BoxCenteredOnMobile py={[3, 3, 4]} mt={4}>
+			<MinMaxScaleExample />
+		</BoxCenteredOnMobile>
+
+		<CodeHighlight
+			code={`
+				const [value, setValue] = useState(0.25)
+
+				// custom limits
+				const min = 0.25
+				const max = 0.75
+
+				// get value within limits
+				const valueWithinLimits = v => Math.min(Math.max(v, min), max)
+
+				// custom range
+				const range = [0, 100]
+
+				// scaled range value
+				const rangeValue = value * (range[1] - range[0]) + range[0]
+
+				return (
+					<CircularInput
+						value={valueWithinLimits(value)}
+						onChange={v => setValue(valueWithinLimits(v))}
+					>
+						<CircularTrack />
+						<CircularProgress />
+
+						{/* limit lines */}
+						<line x1={-10} x2={10} y1={100} y2={100} stroke="black" />
+						<line x1={190} x2={210} y1={100} y2={100} stroke="black" />
+
+						<CircularThumb />
+
+						{/* range value in center */}
+						<text x={100} y={100} textAnchor="middle" dy="0.3em" fontWeight="bold">
+							{Math.round(rangeValue)}%
+						</text>
+					</CircularInput>
+				)
+			`}
+		/>
+
 		<h2 id="readonly">Readonly</h2>
 
 		<p>
@@ -396,3 +321,174 @@ export default () => (
 		</ul>
 	</>
 )
+
+function DefaultExample() {
+	const [value, setValue] = useState(0.25)
+	return (
+		<CircularInput value={value} onChange={setValue}>
+			<CircularTrack />
+			<CircularProgress />
+			<CircularThumb />
+		</CircularInput>
+	)
+}
+
+function ProgressExample() {
+	const [value, setValue] = useState(0.25)
+
+	useEffect(() => {
+		const i = setInterval(() => {
+			setValue(Math.random())
+		}, 1000)
+		return () => clearInterval(i)
+	}, [])
+
+	return (
+		<Spring to={{ value }} config={config.slow}>
+			{props => (
+				<CircularInput value={props.value}>
+					<CircularTrack strokeWidth={5} stroke="#eee" />
+					<CircularProgress stroke={`hsl(${props.value * 100}, 100%, 50%)`} />
+				</CircularInput>
+			)}
+		</Spring>
+	)
+}
+
+const StyledCircularInput = styled(CircularInput)`
+	.custom-progress {
+		stroke: #b1108e;
+		stroke-width: 40px;
+	}
+`
+
+const StyledCircularTrack = styled(CircularTrack)`
+	stroke: #f6f6f6;
+	stroke-width: 10px;
+`
+
+function StyledExample() {
+	const [value, setValue] = useState(0.25)
+	return (
+		<StyledCircularInput value={value} onChange={setValue}>
+			<StyledCircularTrack />
+			<CircularProgress className="custom-progress" />
+			<CircularThumb r={10} fill="rgba(255,255,255,0.5)" />
+		</StyledCircularInput>
+	)
+}
+
+function AnimatedExample() {
+	const [value, setValue] = useState(0.25)
+
+	return (
+		<Spring to={{ value }} config={config.stiff}>
+			{props => (
+				<CircularInput value={props.value} onChange={setValue}>
+					<CircularTrack />
+					<CircularProgress />
+					<CircularThumb />
+				</CircularInput>
+			)}
+		</Spring>
+	)
+}
+
+function CustomComponent() {
+	const { getPointFromValue, value } = useCircularInputContext()
+	const { x, y } = getPointFromValue()
+
+	return (
+		<text
+			x={x}
+			y={y}
+			textAnchor="middle"
+			dy="0.35em"
+			fill="rgb(61, 153, 255)"
+			style={{ pointerEvents: 'none', fontWeight: 'bold' }}
+		>
+			{Math.round(value * 100)}
+		</text>
+	)
+}
+
+function CustomExample() {
+	const [value, setValue] = useState(0.25)
+
+	return (
+		<Spring to={{ value }} config={config.stiff}>
+			{props => (
+				<CircularInput value={props.value} onChange={setValue}>
+					<CircularProgress
+						strokeWidth={45}
+						stroke={`rgba(61, 153, 255, ${props.value})`}
+					/>
+					<CircularThumb
+						fill="white"
+						stroke="rgb(61, 153, 255)"
+						strokeWidth="5"
+					/>
+					<CustomComponent />
+				</CircularInput>
+			)}
+		</Spring>
+	)
+}
+
+function RenderPropExample() {
+	const [value, setValue] = useState(0.25)
+	return (
+		<CircularInput value={value} onChange={setValue}>
+			{({ getPointFromValue }) => (
+				<>
+					<CircularTrack />
+					<CircularProgress />
+					<text {...getPointFromValue()} dx="30" dy="0.3em">
+						wee!
+					</text>
+					<CircularThumb />
+				</>
+			)}
+		</CircularInput>
+	)
+}
+
+function MinMaxScaleExample() {
+	const [value, setValue] = useState(0.25)
+
+	// custom limits
+	const min = 0.25
+	const max = 0.75
+
+	// get value within limits
+	const valueWithinLimits = v => Math.min(Math.max(v, min), max)
+
+	// custom range
+	const range = [0, 100]
+
+	// scaled range value
+	const rangeValue = value * (range[1] - range[0]) + range[0]
+
+	return (
+		<CircularInput
+			// make sure value is always within limits
+			value={valueWithinLimits(value)}
+			// make sure we set value within limits
+			onChange={v => setValue(valueWithinLimits(v))}
+		>
+			<CircularTrack />
+			<CircularProgress />
+
+			{/* limit lines */}
+			<line x1={-10} x2={10} y1={100} y2={100} stroke="black" />
+			<line x1={190} x2={210} y1={100} y2={100} stroke="black" />
+
+			<CircularThumb />
+
+			{/* range value in center */}
+			<text x={100} y={100} textAnchor="middle" dy="0.3em" fontWeight="bold">
+				{Math.round(rangeValue)}%
+			</text>
+		</CircularInput>
+	)
+}
